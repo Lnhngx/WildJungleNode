be2044fa007be8416eac79ae1f10b89554b06df9
Chenyenjui <capricorn15.tw@gmail.com>
2022-03-09 12:45:05 +0800

    tour

--- modules/connect-db.js
+++ modules/connect-db.js
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
     user:process.env.DB_USER,
     password:process.env.DB_PASS,
     database:process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections:true,
     connectionLimit:5, //最大連線數
     queueLimit:0, //排隊限制
