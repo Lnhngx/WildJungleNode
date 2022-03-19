833af8dec8a3675c27b55b6dad1207abcba54088
tony-hsueh <yahooleo36@gmail.com>
2022-03-19 10:45:10 +0800

    改遊戲部分

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ app.get('/productstype', async (req, res) => {
 
 
 app.get('/game', async (req, res) => {
-    const sql = "SELECT `question`.`sid`,`name`,`qcontent`,`acontent`,`yesno` FROM `question` JOIN `answer` WHERE `question_sid` = `question`.`sid`";
+    const sql = "SELECT `question`.`sid`,`name`,`qcontent`,`acontent`,`yesno` FROM `question` JOIN `answer` WHERE `question_sid` = `question`.`sid` LIMIT 40";
 
     const [results, fields] = await db.query(sql);
 
