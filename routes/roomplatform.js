e2ca33fb9da93f43e59baefb028965aa4d22a6a3
Yi-Ning <ning19950212@gmail.com>
2022-03-29 00:28:13 +0800

    新增評論

--- routes/roomplatform.js
+++ routes/roomplatform.js
@@ -6,12 +6,45 @@ const router = express.Router();
 
 // 自訂路由
 
-router.get('/room-comments-list', async (req, res) => {
-    const sql = "SELECT roomplatform.sid , roomplatform.service_score , roomplatform.clean_score , roomplatform.comfort_score , roomplatform.facility_score , roomplatform.cpValue_score, roomplatform.comments , members.m_name , orders_details_live.start  , orders_details_live.end, roomdetail.room_name FROM roomplatform JOIN members on roomplatform.m_sid = members.m_sid JOIN orders_details_live on roomplatform.order_detail_live_sid = orders_details_live.sid JOIN roomdetail on orders_details_live.room_sid = roomdetail.sid ORDER BY orders_details_live.start DESC";
 
-    const [results, fields] = await db.query(sql);
+//海洋房資料
+
+router.get('/room-comments-oceanlist', async (req, res) => {
+    const sql = "SELECT roomplatform.sid , roomplatform.service_score , roomplatform.clean_score , roomplatform.comfort_score , roomplatform.facility_score , roomplatform.cpValue_score, roomplatform.comments , members.m_name , orders_details_live.start  , orders_details_live.end, roomdetail.room_name FROM roomplatform JOIN members on roomplatform.m_sid = members.m_sid JOIN orders_details_live on roomplatform.order_detail_live_sid = orders_details_live.sid JOIN roomdetail on orders_details_live.room_sid = roomdetail.sid WHERE roomdetail.sid=1 ORDER BY orders_details_live.start DESC";
+
+    const [results] = await db.query(sql);
 
     res.json(results);
+})
+
+//冰原房資料
+
+router.get('/room-comments-icelist', async (req, res) => {
+  const sql = "SELECT roomplatform.sid , roomplatform.service_score , roomplatform.clean_score , roomplatform.comfort_score , roomplatform.facility_score , roomplatform.cpValue_score, roomplatform.comments , members.m_name , orders_details_live.start  , orders_details_live.end, roomdetail.room_name FROM roomplatform JOIN members on roomplatform.m_sid = members.m_sid JOIN orders_details_live on roomplatform.order_detail_live_sid = orders_details_live.sid JOIN roomdetail on orders_details_live.room_sid = roomdetail.sid WHERE roomdetail.sid=2 ORDER BY orders_details_live.start DESC";
+
+  const [results] = await db.query(sql);
+
+  res.json(results);
+})
+
+//夜行房資料
+
+router.get('/room-comments-nocturnallist', async (req, res) => {
+  const sql = "SELECT roomplatform.sid , roomplatform.service_score , roomplatform.clean_score , roomplatform.comfort_score , roomplatform.facility_score , roomplatform.cpValue_score, roomplatform.comments , members.m_name , orders_details_live.start  , orders_details_live.end, roomdetail.room_name FROM roomplatform JOIN members on roomplatform.m_sid = members.m_sid JOIN orders_details_live on roomplatform.order_detail_live_sid = orders_details_live.sid JOIN roomdetail on orders_details_live.room_sid = roomdetail.sid WHERE roomdetail.sid=3 ORDER BY orders_details_live.start DESC";
+
+  const [results] = await db.query(sql);
+
+  res.json(results);
+})
+
+//熱帶房資料
+
+router.get('/room-comments-tropicallist', async (req, res) => {
+  const sql = "SELECT roomplatform.sid , roomplatform.service_score , roomplatform.clean_score , roomplatform.comfort_score , roomplatform.facility_score , roomplatform.cpValue_score, roomplatform.comments , members.m_name , orders_details_live.start  , orders_details_live.end, roomdetail.room_name FROM roomplatform JOIN members on roomplatform.m_sid = members.m_sid JOIN orders_details_live on roomplatform.order_detail_live_sid = orders_details_live.sid JOIN roomdetail on orders_details_live.room_sid = roomdetail.sid WHERE roomdetail.sid=4 ORDER BY orders_details_live.start DESC";
+
+  const [results] = await db.query(sql);
+
+  res.json(results);
 })
 
 router.post("/room-comments-post", async (req, res) => {
@@ -22,7 +55,7 @@ router.post("/room-comments-post", async (req, res) => {
   const sql =
     "INSERT INTO `roomplatform`(`service_score`, `clean_score`, `comfort_score`, `facility_score`, `cpValue_score`, `comments` , `m_sid`,`order_sid` , `order_detail_live_sid`) VALUES (?,?,?,?,?,?,?,?,?)";
 
-  const [result,fields] = await db.query(sql, [
+  const [result] = await db.query(sql, [
     req.body.serve,
     req.body.clean,
     req.body.comfort,
@@ -38,6 +71,7 @@ router.post("/room-comments-post", async (req, res) => {
   output.success = !!result.affectedRows;
   output.result = result;
   res.json(output);
+  return res.json(result)
 });
 
 module.exports = router;
